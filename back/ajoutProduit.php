5a91791e3020fdb3982ab89f85811f303dac6ee1
Adrien KOUYOUMJIAN <adrien.kouyoumjian@outlook.fr>
2023-04-27 15:30:47 +0200

    creation du formulaire

--- back/ajoutProduit.php
+++ back/ajoutProduit.php
@@ -2,111 +2,80 @@
 
 <h1 class="text-center mt-5">Ajout produit</h1>
 
-<div class="container mt-3 border border-light">
+<div class="container mt-3 p-3 border border-light rounded">
 
 
-<form>
+<form method="post">
   <fieldset>
-    <legend>Legend</legend>
     <div class="form-group row">
-      <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
+      <label for="titre" class="col-sm-2 col-form-label">Titre</label>
       <div class="col-sm-10">
-        <input type="text" readonly="" class="form-control-plaintext" id="staticEmail" value="email@example.com">
+        <input type="text" readonly="" class="form-control-plaintext" id="titre" name="titre">
       </div>
     </div>
+  
     <div class="form-group">
-      <label for="exampleInputEmail1" class="form-label mt-4">Email address</label>
-      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-      <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-    </div>
-    <div class="form-group">
-      <label for="exampleInputPassword1" class="form-label mt-4">Password</label>
-      <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-    </div>
-    <div class="form-group">
-      <label for="exampleSelect1" class="form-label mt-4">Example select</label>
-      <select class="form-select" id="exampleSelect1">
-        <option>1</option>
-        <option>2</option>
-        <option>3</option>
-        <option>4</option>
-        <option>5</option>
+      <label for="marque" class="form-label mt-4">Marque</label>
+      <select class="form-select" id="marque" name="marque">
+        <option value="adidas">Adidas</option>
+        <option value="nike">Nike</option>
+        <option value="maje">MAJE</option>
       </select>
     </div>
+
     <div class="form-group">
-      <label for="exampleSelect2" class="form-label mt-4">Example multiple select</label>
-      <select multiple="" class="form-select" id="exampleSelect2">
-        <option>1</option>
-        <option>2</option>
-        <option>3</option>
-        <option>4</option>
-        <option>5</option>
+      <label for="matiere" class="form-label mt-4">Matière</label>
+      <select class="form-select" id="matiere" name="matiere">
+        <option value="coton">Coton</option>
+        <option value="synthetique">Synthétique</option>
+        <option value="lin">Lin</option>
       </select>
     </div>
+   
+    
     <div class="form-group">
-      <label for="exampleTextarea" class="form-label mt-4">Example textarea</label>
-      <textarea class="form-control" id="exampleTextarea" rows="3"></textarea>
+      <label for="couleur" class="form-label mt-4">Couleur</label>
+      <select class="form-select" id="couleur" name="couleur">
+        <option value="r">Rouge</option>
+        <option value="g">Vert</option>
+        <option value="b">Bleu</option>
+      </select>
     </div>
+   
+
     <div class="form-group">
-      <label for="formFile" class="form-label mt-4">Default file input example</label>
-      <input class="form-control" type="file" id="formFile">
+      <label for="taille" class="form-label mt-4">Taille</label>
+      <select class="form-select" id="taille" name="taille">
+        <option value="s">Small</option>
+        <option value="m">Medium</option>
+        <option value="l">Large</option>
+      </select>
     </div>
-    <fieldset class="form-group">
-      <legend class="mt-4">Radio buttons</legend>
-      <div class="form-check">
-        <input class="form-check-input" type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked="">
-        <label class="form-check-label" for="optionsRadios1">
-          Option one is this and that—be sure to include why it's great
-        </label>
-      </div>
-      <div class="form-check">
-        <input class="form-check-input" type="radio" name="optionsRadios" id="optionsRadios2" value="option2">
-        <label class="form-check-label" for="optionsRadios2">
-          Option two can be something else and selecting it will deselect option one
-        </label>
-      </div>
-      <div class="form-check disabled">
-        <input class="form-check-input" type="radio" name="optionsRadios" id="optionsRadios3" value="option3" disabled="">
-        <label class="form-check-label" for="optionsRadios3">
-          Option three is disabled
-        </label>
-      </div>
-    </fieldset>
-    <fieldset class="form-group">
-      <legend class="mt-4">Checkboxes</legend>
-      <div class="form-check">
-        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-        <label class="form-check-label" for="flexCheckDefault">
-          Default checkbox
-        </label>
-      </div>
-      <div class="form-check">
-        <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked="">
-        <label class="form-check-label" for="flexCheckChecked">
-          Checked checkbox
-        </label>
-      </div>
-    </fieldset>
-    <fieldset class="form-group">
-      <legend class="mt-4">Switches</legend>
-      <div class="form-check form-switch">
-        <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault">
-        <label class="form-check-label" for="flexSwitchCheckDefault">Default switch checkbox input</label>
-      </div>
-      <div class="form-check form-switch">
-        <input class="form-check-input" type="checkbox" id="flexSwitchCheckChecked" checked="">
-        <label class="form-check-label" for="flexSwitchCheckChecked">Checked switch checkbox input</label>
-      </div>
-    </fieldset>
-    <fieldset class="form-group">
-      <legend class="mt-4">Ranges</legend>
-        <label for="customRange1" class="form-label">Example range</label>
-        <input type="range" class="form-range" id="customRange1">
-        <label for="disabledRange" class="form-label">Disabled range</label>
-        <input type="range" class="form-range" id="disabledRange" disabled="">
-        <label for="customRange3" class="form-label">Example range</label>
-        <input type="range" class="form-range" min="0" max="5" step="0.5" id="customRange3">
-    </fieldset>
+   
+    <div class="form-group">
+      <label for="genre" class="form-label mt-4">Genre</label>
+      <select class="form-select" id="genre" name="genre">
+        <option value="m">Homme</option>
+        <option value="w">Femme</option>
+        <option value="u">Unisexe</option>
+      </select>
+    </div>    
+
+    <div class="form-group">
+      <label for="type" class="form-label mt-4">Type</label>
+      <select class="form-select" id="type" name="type">
+        <option value="t-shirt">t-shirt</option>
+        <option value="pantalon">pantalon</option>
+        <option value="chaussure">chaussure</option>
+      </select>
+    </div>
+
+
+    <div class="form-group">
+      <label for="formFile" class="form-label mt-4">Prix</label>
+      <input class="form-control" type="number" id="prix" min="0" step="0.01" name="prix">
+    </div>
+   
     <button type="submit" class="btn btn-primary">Submit</button>
   </fieldset>
 </form>
