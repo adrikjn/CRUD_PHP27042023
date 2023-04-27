c9563683c9a43741bd606d64576dbdeb3401ebc5
Adrien KOUYOUMJIAN <adrien.kouyoumjian@outlook.fr>
2023-04-27 15:57:19 +0200

    push formulaire final

--- back/ajoutProduit.php
+++ back/ajoutProduit.php
@@ -3,85 +3,75 @@
 <h1 class="text-center mt-5">Ajout produit</h1>
 
 <div class="container mt-3 p-3 border border-light rounded">
-
-
 <form class="p-5" method="post">
   <fieldset>
     <div class="form-group">
-      <label for="titre" class="col-sm-2 col-form-label">Titre</label>
-        <input type="text"  class="form-control" id="titre" name="titre">
+      <label for="titre" class="col-sm-2 col-form-label">titre</label>
+    <input type="text" class="form-control" id="titre" name="titre">
+      
     </div>
-  
+
     <div class="form-group">
       <label for="marque" class="form-label mt-4">Marque</label>
       <select class="form-select" id="marque" name="marque">
+        <option class="text-center">---Selectionnez une marque---</option>
         <option value="adidas">Adidas</option>
-        <option value="nike">Nike</option>
-        <option value="maje">MAJE</option>
+        <option value="Nike">Nike</option>
+        <option value='Maje'>Maje</option>
       </select>
     </div>
-
     <div class="form-group">
       <label for="matiere" class="form-label mt-4">Matière</label>
       <select class="form-select" id="matiere" name="matiere">
+      <option class="text-center">---Selectionnez une matière---</option>
         <option value="coton">Coton</option>
         <option value="synthetique">Synthétique</option>
-        <option value="lin">Lin</option>
+        <option value='lin'>Lin</option>
       </select>
     </div>
-   
-    
     <div class="form-group">
       <label for="couleur" class="form-label mt-4">Couleur</label>
       <select class="form-select" id="couleur" name="couleur">
+      <option class="text-center">---Selectionnez une couleur---</option>
         <option value="r">Rouge</option>
         <option value="g">Vert</option>
-        <option value="b">Bleu</option>
+        <option value='b'>Bleu</option>
       </select>
     </div>
-   
-
     <div class="form-group">
       <label for="taille" class="form-label mt-4">Taille</label>
       <select class="form-select" id="taille" name="taille">
+      <option class="text-center">---Selectionnez une taille---</option>
         <option value="s">Small</option>
         <option value="m">Medium</option>
-        <option value="l">Large</option>
+        <option value='l'>Large</option>
       </select>
     </div>
-   
     <div class="form-group">
       <label for="genre" class="form-label mt-4">Genre</label>
       <select class="form-select" id="genre" name="genre">
+      <option class="text-center">---Selectionnez un genre---</option>
         <option value="m">Homme</option>
         <option value="w">Femme</option>
-        <option value="u">Unisexe</option>
+        <option value='u'>Unisexe</option>
       </select>
-    </div>    
-
+    </div>
     <div class="form-group">
       <label for="type" class="form-label mt-4">Type</label>
       <select class="form-select" id="type" name="type">
+      <option class="text-center">---Selectionnez un type---</option>
         <option value="t-shirt">t-shirt</option>
         <option value="pantalon">pantalon</option>
-        <option value="chaussure">chaussure</option>
+        <option value='chaussure'>chaussure</option>
       </select>
     </div>
-
-
     <div class="form-group">
-      <label for="formFile" class="form-label mt-4">Prix</label>
-      <input class="form-control" type="number" id="prix" min="0" step="0.01" name="prix">
+      <label for="prix" class="form-label mt-4">Prix</label>
+      <input class="form-control" type="number" min="0" step="0.01" id="prix" name="prix">
     </div>
-   
-    <button type="submit" class="btn btn-primary mt-3">Submit</button>
+    
+    <button type="submit" class="btn btn-primary">ajout produit</button>
   </fieldset>
 </form>
-
 </div>
-
 <?php require_once('../inclusion/footer.inc.php'); ?>
-
-
-
-
